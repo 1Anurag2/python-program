6880678d61f738d5a8a6f7aaf8c69af3a8b1cf02
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2024-08-30 22:38:10 +0530

    .

---
+++ Questions/divide_two_integers.py
@@ -0,0 +1,7 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        multipicant = dividend/divisor
+        return multipicant
+        
+sol = Solution()
+print(int(sol.divide(7,-3)))
